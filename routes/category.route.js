fd7b59ac6cbd31e262e3e15d0a8ca37e7a36f305
unknown <visingiriev@mail.ru>
2021-11-29 16:46:04 +0300

    prettier

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,11 +1,11 @@
-const {Router} = require("express")
-const {categoriesController} = require("../controlllers/category.controller");
+const { Router } = require("express");
+const { categoriesController } = require("../controlllers/category.controller");
 
-const router = Router()
+const router = Router();
 
-router.post("/add", categoriesController.addCategory)
-router.get("/", categoriesController.getAllCategories)
-router.delete("/remove/:id", categoriesController.removeCategory)
-router.patch("/update/:id", categoriesController.editCategory)
+router.post("/add", categoriesController.addCategory);
+router.get("/", categoriesController.getAllCategories);
+router.delete("/remove/:id", categoriesController.removeCategory);
+router.patch("/update/:id", categoriesController.editCategory);
 
 module.exports = router;
