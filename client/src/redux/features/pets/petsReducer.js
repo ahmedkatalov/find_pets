3a753936665e5fea1f8777d466699f2a26829c2e
musa <mediggggh@gmail.com>
2022-02-15 16:30:15 +0300

    кавычки

--- client/src/redux/features/pets/petsReducer.js
+++ client/src/redux/features/pets/petsReducer.js
@@ -18,13 +18,13 @@ export const petsReducer = (state = initialState, action) => {
         pending: false,
       };
 
-    case 'pets/content/pending' :
+    case "pets/content/pending" :
       return {
         ...state,
         pending: false
       }
 
-    case 'content/pets/fulfilled':
+    case "content/pets/fulfilled":
       return {
         ...state,
         pets: action.payload,
@@ -39,60 +39,3 @@ export const petsReducer = (state = initialState, action) => {
       return state;
   }
 };
-
-export const fetchPets = () => {
-  return (dispatch) => {
-    dispatch({ type: "pets/load/pending" });
-    fetch("/pets")
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch({ type: "pets/load/fulfilled", payload: data });
-      });
-  };
-};
-
-export const uploadPets = (header, description, category, file) => {
-  return (dispatch) => {
-    dispatch({ type: "pets/load/pending" });
-    const formData = new FormData();
-    formData.append("img", file);
-    formData.append("header", header);
-    formData.append("description", description);
-    formData.append("category", category);
-
-    fetch("/pets/add", {
-      method: "POST",
-      headers: {
-        // "Content-type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("token")}`
-      },
-      body: formData
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  };
-};
-
-export const fetchContentPets = () => {
-  return (dispatch) => {
-    dispatch({type: 'pets/content/pending'})
-    fetch('/pets/')
-    .then(res => res.json())
-    .then(data => {
-      dispatch({type: 'content/pets/fulfilled', payload:data})
-    })
-  }
-}
-
-export const removeCardPet = (id) =>{
-  return(dispatch) => {
-    fetch(`/pets/remove/${id}`, {
-      method: "DELETE"
-    })
-        .then((res) => res.json())
-        .then((data) => {
-          dispatch({type: "pets/remove/fulfilled", payload: id})
-        })
-
-  }
-}
